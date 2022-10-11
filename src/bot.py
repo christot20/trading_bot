9e10c61a8c0383eba5f66fb752638d0c98ccd5b3
christot20 <thomaschristo1234@yahoo.com>
2022-10-11 14:06:51 -0400

    added way to monitor transcations

--- src/bot.py
+++ src/bot.py
@@ -2,9 +2,14 @@ from alpaca.trading.client import TradingClient
 from alpaca.data.historical import StockHistoricalDataClient
 from src.config import Reddit_ALPACA_API_KEY, Reddit_ALPACA_SECRET_KEY, Algo_ALPACA_API_KEY, Algo_ALPACA_SECRET_KEY, Neural_ALPACA_API_KEY, Neural_ALPACA_SECRET_KEY
 from src.trading_methods import methods
+import time
 
 # look at how to set up your code/organize like that guy did with ai bot
 # start trying to have it make trades as a test and try to learn how to use an AI for it later
+def keep_running():
+    while True:
+        time.sleep(1)
+
 def main():
     # paper=True enables paper trading
     trader = methods()    
@@ -19,14 +24,17 @@ def main():
         trading_client = TradingClient(Reddit_ALPACA_API_KEY, Reddit_ALPACA_SECRET_KEY, paper=True) # each has different keys since they are on different accounts
         stock_client = StockHistoricalDataClient(Reddit_ALPACA_API_KEY,  Reddit_ALPACA_SECRET_KEY)
         trader.reddit_mode(trading_client, stock_client)
+        keep_running() # used to monitor activity manually
     elif choice == "a":
         trading_client = TradingClient(Algo_ALPACA_API_KEY, Algo_ALPACA_SECRET_KEY, paper=True)
         stock_client = StockHistoricalDataClient(Algo_ALPACA_API_KEY,  Algo_ALPACA_SECRET_KEY)
         trader.algo_mode(trading_client, stock_client)
+        keep_running()
     else:
         trading_client = TradingClient(Neural_ALPACA_API_KEY, Neural_ALPACA_SECRET_KEY, paper=True)
         stock_client = StockHistoricalDataClient(Neural_ALPACA_API_KEY,  Neural_ALPACA_SECRET_KEY)
         trader.neural_mode(trading_client, stock_client)
+        keep_running()
 
 if __name__  == "__main__":
     main()
