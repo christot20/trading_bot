2e9faa04f2f1ab2eb275062ea4327cd2d085cccc
christot20 <thomaschristo1234@yahoo.com>
2022-10-17 01:51:47 -0400

    removed unnecessary scripts for one functioning script

--- shell_scripts/bot_runner.ps1
+++ shell_scripts/bot_runner.ps1
@@ -1,8 +1,8 @@
-# make if statement to check if the day is not saturday or sunday
+# used for if statement to check if the day is not saturday or sunday or holiday
 $Day = (get-date).DayOfWeek
 $Date = get-date -DisplayHint Date
 $HolidayTable = @(
-    # 2022/2023 US Federal Holidays (From Sepetmber 2022 - December 2023)
+    # 2022/2023 US Federal Holidays (From September 2022 - December 2023)
     'Monday, September 5, 2022',
     'Thursday, November 24, 2022',
     'Monday, December 26, 2023',
@@ -16,26 +16,27 @@ $HolidayTable = @(
     'Monday, September 4, 2023',
     'Thursday, November 23, 2023',
     'Monday, December 25, 2023'
-);
+)
 
-if (($Day -ne "Saturday" -and $Day -ne "Sunday") -and ($Date -notin $HolidayTable))
+$wshell = New-Object -ComObject wscript.shell # used for keyboard input
+if (($Day -ne "Saturday" -and $Day -ne "Sunday") -and ($Date -notin $HolidayTable)) # if it is trading day
 {
-    $choices = 'm','a','r'
+    $choices = 'm','a','r' # bot choices
     $counter = 0
     Foreach ($choice in $choices)
     {
-        Start-Process "c:/trading_bot/venv/Scripts/python.exe" -ArgumentList "c:/trading_bot/src/bot.py"
-        $wshell = New-Object -ComObject wscript.shell
-        Start-Sleep 30
-        $wshell.SendKeys($choice) # make something so that it takes an input on what to choose (r, a, or m)
-        $wshell.SendKeys('{ENTER}')
+        Start-Process "c:/trading_bot/venv/Scripts/python.exe" -ArgumentList "c:/trading_bot/src/bot.py" # start script
+        Start-Sleep 30 # wait for everything to load
+        $wshell.SendKeys($choice) # used to input what to choose (r, a, or m)
+        $wshell.SendKeys('{ENTER}') 
         if ($counter -eq 0)
         {
-            Start-Sleep ((get-date "09:30am") - (get-date)).TotalSeconds
+            Start-Sleep ((get-date "09:30am") - (get-date)).TotalSeconds # for first choice, machine learning, wait until 9:30 to perform next methods
         }
         $counter++
     }
-    Start-Sleep ((get-date "11:00am") - (get-date)).TotalSeconds
-    Stop-Process -Name "Python" # works
+    Start-Sleep ((get-date "11:00am") - (get-date)).TotalSeconds # wait for everything to be done by 11 at latest
+    Stop-Process -Name "Python" # close python
 }
-exit
+$wshell.SendKeys('exit') # exit the window
+$wshell.SendKeys('{ENTER}')
