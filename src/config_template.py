998c6664fb4baac600af55512d633503ceb099ee
christot20 <thomaschristo1234@yahoo.com>
2022-11-01 23:14:54 -0400

    added config file template

---
+++ src/config_template.py
@@ -0,0 +1,21 @@
+# # API info for each account
+# Reddit_ALPACA_API_KEY = 
+# Reddit_ALPACA_SECRET_KEY = 
+
+# Algo_ALPACA_API_KEY = 
+# Algo_ALPACA_SECRET_KEY = 
+
+# Neural_ALPACA_API_KEY = 
+# Neural_ALPACA_SECRET_KEY = 
+
+# # MYSQL/SQL database password
+# db_password = 
+
+# # IEX Cloud Token
+# IEX_CLOUD_API_TOKEN = 
+
+# # Server used to check if internet connection is on
+# REMOTE_SERVER = 
+
+# # Path to root of trading bot project
+# PROJECT_PATH = 
